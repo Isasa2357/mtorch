d5a9141c80c37637d8b76373845c608a6a0906ae
Isasa2357 <iwatake.s.b573@m.isct.ac.jp>
2025-08-16 19:17:38 +0900

    Atariモジュールを作成

--- util/networkUpdate.py
+++ util/networkUpdate.py
@@ -4,9 +4,15 @@ from copy import copy, deepcopy
 from torch import nn
 
 def hard_update(source: nn.Module, target: nn.Module):
+    '''
+    ハードアップデート
+    '''
     target = deepcopy(source)
 
 def soft_update(source: nn.Module, target: nn.Module, tau: float):
+    '''
+    ソフトアップデート
+    '''
     for target_param, source_param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(tau * source_param.data + (1.0 - tau) * target_param.data)
     
